a45f93f551ede2e6b41a9c93d0eb3b9f9b931bf3
Iam-Meng <pochseavmeng@gmail.com>
2026-03-29 10:51:29 +0700

    idk

--- W10 - Start code/Start code/src/components/Log.jsx
+++ W10 - Start code/Start code/src/components/Log.jsx
@@ -3,7 +3,10 @@ function Log({ isPlayer, isDamage, text }) {
     return (
         <section id="log" className="container">
             <h2>Battle Log</h2>
-            
+            <p className={isPlayer ? "player" : "enemy"}>
+                {isDamage ? "Damage: " : "Heal: "}
+                {text}
+            </p>
         </section>
     );
 }
